3b279d3dfa5fe386d02f4f6854162960230982ba
Rodrigo Detomini <rdetomini@hotmail.com>
2019-04-23 20:01:29 -0300

    Inserindo Identity Core no Dotnet

--- ProAgil.WebAPI/Helpers/AutoMapperProfiles.cs
+++ ProAgil.WebAPI/Helpers/AutoMapperProfiles.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using AutoMapper;
 using ProAgil.Domain;
+using ProAgil.Domain.Identity;
 using ProAgil.WebAPI.Dtos;
 
 namespace ProAgil.WebAPI.Helpers
@@ -22,6 +23,8 @@ namespace ProAgil.WebAPI.Helpers
 
             CreateMap<Lote, LoteDto>().ReverseMap();
             CreateMap<RedeSocial, RedeSocialDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, UserLoginDto>().ReverseMap();
         }
     }
 }
